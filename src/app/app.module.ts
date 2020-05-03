ab26fc0d8b8e8d38e8cbc03869deebf7882da350
MKortmann <companymvms@gmail.com>
2020-05-03 18:36:32 +0200

    add: sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+
+import { SidebarModule } from 'ng-sidebar';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, NavComponent, SidenavComponent],
+  imports: [BrowserModule, SidebarModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
