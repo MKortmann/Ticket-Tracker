906e496010292f5983b109b02ce6be2569d02ddf
MKortmann <companymvms@gmail.com>
2020-05-03 19:52:19 +0200

    add: d3 and first tests of user-dashboard

--- src/app/components/user-dashboard/user-dashboard.component.ts
+++ src/app/components/user-dashboard/user-dashboard.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as d3 from 'd3';
 @Component({
   selector: 'app-user-dashboard',
   templateUrl: './user-dashboard.component.html',
-  styleUrls: ['./user-dashboard.component.css']
+  styleUrls: ['./user-dashboard.component.css'],
 })
 export class UserDashboardComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-  }
+    d3.select('p').style('color', 'red');
 
+    d3.select('p').append('p').text('Appended paragrah');
+    d3.select('p').append('p').text('Appended paragrah');
+    d3.select('p').append('p').text('Appended paragrah');
+
+    d3.selectAll('p').style('color', 'blue');
+  }
 }
