00386ed9fe0ffc467d79b4c21d8a56aea74794f6
MKortmann <companymvms@gmail.com>
2020-05-03 19:03:36 +0200

    add/stöye: push sidenav function + improve style / add: dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { NavComponent } from './components/nav/nav.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
 
 import { SidebarModule } from 'ng-sidebar';
+import { UserDashboardComponent } from './components/user-dashboard/user-dashboard.component';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, SidenavComponent],
+  declarations: [AppComponent, NavComponent, SidenavComponent, UserDashboardComponent],
   imports: [BrowserModule, SidebarModule],
   providers: [],
   bootstrap: [AppComponent],
